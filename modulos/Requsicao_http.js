8f94329f018ea5c95d90cc5acad8c86dd0e9114e
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-01-10 07:38:59 -0300

    Create Requsicao_http.js

---
+++ modulos/Requsicao_http.js
@@ -0,0 +1,24 @@
+// npm i axios
+
+
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+// axios faz requisições http
+const axios = require('axios')
+// Mulher chinesa com menor salário?
+const chineses = f => f.pais ==='China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+
+    const func = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+    console.log(func)
+})
+
