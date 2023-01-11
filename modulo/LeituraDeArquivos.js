78653d475018082c7c779cd4d5f8a5fa53127d54
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-01-11 08:59:01 -0300

    Create LeituraDeArquivos.js

---
+++ modulo/LeituraDeArquivos.js
@@ -0,0 +1,33 @@
+// arquivo.json
+{
+    "db": {
+        "host": "localhost",
+        "port": 5432,
+        "user": "usuario",
+        "pass": "123456"
+    }
+
+}
+**************
+const fs = require('fs')
+const caminho =  __dirname + '/arquivo.json'
+
+//sincrono...
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+console.log(conteudo)
+
+//assincrono = utilizando callback
+fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+    const config = JSON.parse(conteudo)
+    console.log(`${config.db.host}:${config.db.port}`)
+}) // só vai funcionar se o  conteudo for um OBJETO;
+
+// outro método
+const config = require('./arquivo.json')
+console.log(config.db)
+
+// lendo todos arquivos da pasta atual
+fs.readdir(__dirname, (err,arquivos) => {
+    console.log('Conteúdo da pasta....')
+    console.log(arquivos)
+})  
